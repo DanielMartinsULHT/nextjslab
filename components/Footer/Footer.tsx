44f070c2daedd5cd7c94829f7867a2717b9ac54a
Daniel Martins <a22402961@alunos.ulht.pt>
2024-12-04 20:28:54 +0000

    Header and footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+//import { styles } from "./Footer.module.css";
+
+export default function Footer() {
+  return <footer className='bg-transparent text-white font-bold p-10 text-center'>
+    2024 © Daniel Martins
+  </footer>
+}
---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Header() {
+ return (
+    <header className='p-10px bg-transparent font-bold'>
+        <h1>React & Next.js</h1>
+        <nav className=''>
+            <Link href='/'>Home </Link>
+
+        </nav>
+    </header>
+  )
+}
