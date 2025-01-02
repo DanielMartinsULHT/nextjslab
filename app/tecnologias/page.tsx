53c304f0131fbb27bfaf2011c0f88e07e50b27c4
Daniel Martins <a22402961@alunos.ulht.pt>
2025-01-02 10:26:01 +0000

    pagina tecnologias JSON

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import tecnologias from '@/app/data/tecnologias.json';
 
-export default function page() {
+export default function Page() {
   return (
-    <div>Tecnologias</div>
-  )
+    <div>
+      <h1>Tecnologias</h1>
+      <ul>
+        {tecnologias.tecnologias.map((tech, index) => (
+          <li key={index}>
+            <h2>{tech.title}</h2>
+            <img src={tech.image} alt={tech.title} style={{ width: '100px', height: '100px' }} />
+            <p>{tech.description}</p>
+            <p>Rating: {tech.rating}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
