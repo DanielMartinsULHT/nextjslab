cfa8fc08d1376ba2a46882d1c3d49c43dfa0afea
Daniel Martins <a22402961@alunos.ulht.pt>
2025-01-05 00:05:15 +0000

    Corrigindo erro de uma prop nao ser utilizada...

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -3,7 +3,7 @@
 import { Product } from '@/models/interface';
 import React from 'react';
 
-export default function ProductCard({ id, title, price, description, category, image, rating }: Product) {
+export default function ProductCard({ title, price, description, category, image, rating }: Product) {
   return (
     <div style={{ 
       border: '1px solid #ccc', 
